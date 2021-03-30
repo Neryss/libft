36e23a7bbed0740101d33ea4aa9f21b8a401c428
ckurt <ckurt@student.42lyon.fr>
2021-03-30 17:38:51 +0200

    idk

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/01 13:27:28 by ckurt             #+#    #+#             */
-/*   Updated: 2021/03/26 11:55:42 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2021/03/30 15:50:17 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
+	if (!s1 || !s2)
+		return (s1 - s2);
 	while (*s1 == *s2++)
 		if (*s1++ == 0)
 			return (0);
